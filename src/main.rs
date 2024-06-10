e59a7da89580dbe84457744e96ecc4efd23e9881
Chisenon <kamiru.darkness.contractor@gmail.com>
2024-06-10 12:05:30 +0900

    待機中がわかるようにしたぞ！

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #![no_std]
 #![no_main]
 
-use core::pin;
-
-use futures::stream::SelectWithStrategy;
 use panic_halt as _;
 use rp2040_hal as hal;
 use hal::pac;
@@ -45,45 +42,50 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // 16~18番ピンを出力にする
+    // Initialize pins 16, 17, 18 & 25 as outputs
     let mut pin_16 = pins.gpio16.into_push_pull_output();
     let mut pin_17 = pins.gpio17.into_push_pull_output();
     let mut pin_18 = pins.gpio18.into_push_pull_output();
+    let mut pin_25 = pins.gpio25.into_push_pull_output();
 
 
-    // 
+    // Initialize switch pins as inputs with pull-up resistors
     let switch_blue = pins.gpio14.into_pull_up_input();
     let switch_red = pins.gpio15.into_pull_up_input();
-
     let switch_up = pins.gpio0.into_pull_up_input();
 
-
     loop {
-        if switch_blue.is_low().unwrap() {
+        let blue_active = switch_blue.is_low().unwrap();
+        let red_active = switch_red.is_low().unwrap();
+        let up_active = switch_up.is_low().unwrap();
+    
+        if blue_active {
             pin_18.set_high().unwrap();
-            delay.delay_ms(50);
-            pin_18.set_low().unwrap();
-            delay.delay_ms(50);
         } else {
             pin_18.set_low().unwrap();
         }
-
-        if switch_red.is_low().unwrap() {
+    
+        if red_active {
             pin_17.set_high().unwrap();
-            delay.delay_ms(50);
-            pin_17.set_low().unwrap();
-            delay.delay_ms(50);
         } else {
             pin_17.set_low().unwrap();
         }
-
-        if switch_up.is_low().unwrap() {
+    
+        if up_active {
             pin_16.set_high().unwrap();
-            delay.delay_ms(50);
-            pin_16.set_low().unwrap();
-            delay.delay_ms(50);
         } else {
             pin_16.set_low().unwrap();
         }
+    
+        if !blue_active && !red_active && !up_active {
+            pin_25.set_high().unwrap();
+            delay.delay_ms(100);
+            pin_25.set_low().unwrap();
+            delay.delay_ms(100);
+        } else {
+            pin_25.set_low().unwrap();
+        }
+    
+        delay.delay_ms(10);
     }
 }
